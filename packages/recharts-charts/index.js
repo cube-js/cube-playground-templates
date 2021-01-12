3286eb7551af722b33d1adab11555467a893cdf1
A.Vasilev <vaspiring@gmail.com>
2021-01-12 11:31:39 +0300

    fix: recharts dependency

--- packages/recharts-charts/index.js
+++ packages/recharts-charts/index.js
@@ -3,7 +3,14 @@ const {
   ChartRendererSnippet,
 } = require('@cubejs-templates/core');
 
-class RechartsChartsTemplate extends TemplatePackage {}
+class RechartsChartsTemplate extends TemplatePackage {
+  importDependencies() {
+    return {
+      ...super.importDependencies(),
+      recharts: '2.0.0-beta.8',
+    };
+  }
+}
 
 module.exports = (context) =>
   new RechartsChartsTemplate(context, {
