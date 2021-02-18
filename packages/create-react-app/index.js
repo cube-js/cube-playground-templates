d5cab4879df9c25323f2b6011519df363de07307
A.Vasilev <vaspiring@gmail.com>
2021-02-18 13:56:02 +0300

    fix: try global create-react-app

--- packages/create-react-app/index.js
+++ packages/create-react-app/index.js
@@ -13,20 +13,25 @@ class CreateReactAppTemplate extends TemplatePackage {
     const isInstalled = this.appContainer.getPackageVersions()[this.name];
 
     if (!isInstalled) {
-      await this.appContainer
-        .executeCommand('npx', [
+      try {
+        await this.appContainer.executeCommand('npx', [
           'create-react-app',
           this.appContainer.appPath,
           '--use-npm',
-        ])
-        .catch((e) => {
-          if (e.toString().indexOf('ENOENT') !== -1) {
-            throw new Error(
-              `npx is not installed. Please update your npm: \`$ npm install -g npm\`.`
-            );
-          }
-          throw e;
-        });
+        ]);
+      } catch (error) {
+        if (error.toString().indexOf('ENOENT') !== -1) {
+          throw new Error(
+            `npx is not installed. Please update your npm: \`$ npm install -g npm\`.`
+          );
+        }
+
+        // create-react-app may be installed globally, let's try
+        await this.appContainer.executeCommand('create-react-app', [
+          this.appContainer.appPath,
+          '--use-npm',
+        ]);
+      }
     }
   }
 
