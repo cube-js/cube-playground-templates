fe99bd0ca9ab61227018ce967dceccdef1d38ba5
A.Vasilev <vaspiring@gmail.com>
2021-03-12 14:36:42 +0300

    ng postinstall error workaround

--- packages/create-ng-app/index.js
+++ packages/create-ng-app/index.js
@@ -1,17 +1,47 @@
 const { TemplatePackage } = require('../templates-core');
 const fs = require('fs-extra');
 const path = require('path');
+const isDocker = require('is-docker');
 
 class CreateNgAppTemplate extends TemplatePackage {
   async onBeforeApply() {
     const isInstalled = this.appContainer.getPackageVersions()[this.name];
 
     if (!isInstalled) {
-      await this.appContainer
-        .executeCommand(
-          'npx',
+      if (!isDocker()) {
+        await this.appContainer
+          .executeCommand(
+            'npx',
+            [
+              '@angular/cli',
+              'new',
+              path.basename(this.appContainer.appPath),
+              '--routing=false',
+              '--style=css',
+              '--minimal=true',
+              '--packageManager=npm',
+              '--strict=false',
+            ],
+            {
+              cwd: path.dirname(this.appContainer.appPath),
+            }
+          )
+          .catch((e) => {
+            if (e.toString().indexOf('ENOENT') !== -1) {
+              throw new Error(
+                `npx is not installed. Please update your npm: \`$ npm install -g npm\`.`
+              );
+            }
+            throw e;
+          });
+      } else {
+        // a workaround to avoid `Error: Cannot find module '../node_modules/@angular/cli/bin/postinstall/script.js'` until it gets fixed
+        await this.appContainer.executeCommand(
+          'echo n | npm install -g @angular/cli'
+        );
+        await this.appContainer.executeCommand(
+          'ng',
           [
-            '@angular/cli',
             'new',
             path.basename(this.appContainer.appPath),
             '--routing=false',
@@ -23,15 +53,8 @@ class CreateNgAppTemplate extends TemplatePackage {
           {
             cwd: path.dirname(this.appContainer.appPath),
           }
-        )
-        .catch((e) => {
-          if (e.toString().indexOf('ENOENT') !== -1) {
-            throw new Error(
-              `npx is not installed. Please update your npm: \`$ npm install -g npm\`.`
-            );
-          }
-          throw e;
-        });
+        );
+      }
     }
   }
 
