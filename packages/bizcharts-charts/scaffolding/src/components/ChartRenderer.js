48206ee582377d9f153035626088ca0d3881c663
Alex Vasilev <vaspiring@gmail.com>
2021-06-24 19:11:17 +0300

    perf: chart renderer improvements

--- packages/bizcharts-charts/scaffolding/src/components/ChartRenderer.js
+++ packages/bizcharts-charts/scaffolding/src/components/ChartRenderer.js
@@ -1,4 +1,8 @@
+import React from 'react';
 import { Chart, Axis, Tooltip, Geom, PieChart } from 'bizcharts';
+import { Row, Col, Statistic, Table } from 'antd';
+
+import { useDeepMemo } from '../../../hooks';
 
 const stackedChartData = (resultSet) => {
   const data = resultSet
@@ -11,80 +15,165 @@ const stackedChartData = (resultSet) => {
       }))
     )
     .reduce((a, b) => a.concat(b), []);
-
   return data;
+};
+
+const LineChartRenderer = ({ resultSet }) => {
+  const data = useDeepMemo(() => stackedChartData(resultSet), [resultSet]);
+
+  return (
+    <Chart
+      scale={{
+        x: {
+          tickCount: 8,
+        },
+      }}
+      autoFit
+      height={400}
+      data={data}
+      forceFit
+    >
+      <Axis name="x" />
+      <Axis name="measure" />
+      <Tooltip
+        crosshairs={{
+          type: 'y',
+        }}
+      />
+      <Geom type="line" position="x*measure" size={2} color="color" />
+    </Chart>
+  );
+};
+
+const BarChartRenderer = ({ resultSet }) => {
+  const data = useDeepMemo(() => stackedChartData(resultSet), [
+    resultSet.serialize(),
+  ]);
+
+  return (
+    <Chart
+      scale={{
+        x: {
+          tickCount: 8,
+        },
+      }}
+      autoFit
+      height={400}
+      data={data}
+      forceFit
+    >
+      <Axis name="x" />
+      <Axis name="measure" />
+      <Tooltip />
+      <Geom type="interval" position="x*measure" color="color" />
+    </Chart>
+  );
+};
+
+const AreaChartRenderer = ({ resultSet }) => {
+  const data = useDeepMemo(() => stackedChartData(resultSet), [
+    resultSet.serialize(),
+  ]);
+
+  return (
+    <Chart
+      scale={{
+        x: {
+          tickCount: 8,
+        },
+      }}
+      autoFit
+      height={400}
+      data={data}
+      forceFit
+    >
+      <Axis name="x" />
+      <Axis name="measure" />
+      <Tooltip
+        crosshairs={{
+          type: 'y',
+        }}
+      />
+      <Geom
+        type="area"
+        adjust="stack"
+        position="x*measure"
+        size={2}
+        color="color"
+      />
+    </Chart>
+  );
+};
+
+const PieChartRenderer = ({ resultSet }) => {
+  const [data, angleField] = useDeepMemo(() => {
+    return [resultSet.chartPivot(), resultSet.series()];
+  }, [resultSet]);
+
+  return (
+    <PieChart
+      data={data}
+      radius={0.8}
+      angleField={angleField[0].key}
+      colorField="x"
+      label={{
+        visible: true,
+        offset: 20,
+      }}
+      legend={{
+        position: 'bottom',
+      }}
+    />
+  );
+};
+
+const TableRenderer = ({ resultSet, pivotConfig }) => {
+  const [tableColumns, dataSource] = useDeepMemo(() => {
+    return [
+      resultSet.tableColumns(pivotConfig),
+      resultSet.tablePivot(pivotConfig),
+    ];
+  }, [resultSet, pivotConfig]);
+
+  return (
+    <Table pagination={false} columns={tableColumns} dataSource={dataSource} />
+  );
 };
 
 const TypeToChartComponent = {
   line: ({ resultSet }) => {
-    return (
-      <Chart
-        scale={{ x: { tickCount: 8 } }}
-        autoFit
-        height={400}
-        data={stackedChartData(resultSet)}
-        forceFit
-      >
-        <Axis name="x" />
-        <Axis name="measure" />
-        <Tooltip crosshairs={{ type: 'y' }} />
-        <Geom type="line" position="x*measure" size={2} color="color" />
-      </Chart>
-    );
+    return <LineChartRenderer resultSet={resultSet} />;
   },
   bar: ({ resultSet }) => {
-    return (
-      <Chart
-        scale={{ x: { tickCount: 8 } }}
-        autoFit
-        height={400}
-        data={stackedChartData(resultSet)}
-        forceFit
-      >
-        <Axis name="x" />
-        <Axis name="measure" />
-        <Tooltip />
-        <Geom type="interval" position="x*measure" color="color" />
-      </Chart>
-    );
+    return <BarChartRenderer resultSet={resultSet} />;
   },
   area: ({ resultSet }) => {
-    return (
-      <Chart
-        scale={{ x: { tickCount: 8 } }}
-        autoFit
-        height={400}
-        data={stackedChartData(resultSet)}
-        forceFit
-      >
-        <Axis name="x" />
-        <Axis name="measure" />
-        <Tooltip crosshairs={{ type: 'y' }} />
-        <Geom
-          type="area"
-          adjust="stack"
-          position="x*measure"
-          size={2}
-          color="color"
-        />
-      </Chart>
-    );
+    return <AreaChartRenderer resultSet={resultSet} />;
   },
   pie: ({ resultSet }) => {
+    return <PieChartRenderer resultSet={resultSet} />;
+  },
+  number: ({ resultSet }) => {
     return (
-      <PieChart
-        data={resultSet.chartPivot()}
-        radius={0.8}
-        angleField={resultSet.series()[0].key}
-        colorField="x"
-        label={{
-          visible: true,
-          offset: 20,
+      <Row
+        type="flex"
+        justify="center"
+        align="middle"
+        style={{
+          height: '100%',
         }}
-        legend={{
-          position: 'bottom',
-        }}
-      />
+      >
+        <Col>
+          {resultSet.seriesNames().map((s) => (
+            <Statistic value={resultSet.totalRow()[s.key]} />
+          ))}
+        </Col>
+      </Row>
     );
   },
+  table: ({ resultSet, pivotConfig }) => {
+    return <TableRenderer resultSet={resultSet} pivotConfig={pivotConfig} />;
+  },
 };
+
+export default TypeToChartComponent;
--- packages/react-charts/index.js
+++ packages/react-charts/index.js
@@ -1,6 +1,12 @@
 const { TemplatePackage, AppSnippet } = require('@cubejs-templates/core');
 
-class ReactChartsTemplate extends TemplatePackage {}
+class ReactChartsTemplate extends TemplatePackage {
+  importDependencies() {
+    return {
+      'react-chartjs-2': '^2.11.2',
+    };
+  }
+}
 
 module.exports = (context) =>
   new ReactChartsTemplate(context, {
