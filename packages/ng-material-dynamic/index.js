2069c6305aa873dbfd278b78dac61ec21061a21a
Alex Vasilev <vaspiring@gmail.com>
2021-11-11 22:17:03 +0300

    fix: hardcode ng graphql

--- packages/ng-material-dynamic/index.js
+++ packages/ng-material-dynamic/index.js
@@ -5,7 +5,7 @@ class NgMaterialDynamicTemplate extends TemplatePackage {
     return {
       ...super.importDependencies(),
       '@apollo/client': 'latest',
-      graphql: 'latest',
+      graphql: '15.7.2',
       'graphql-tools': '5.0.0',
     };
   }
